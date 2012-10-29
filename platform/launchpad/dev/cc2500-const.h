1356915596799a87f8458e80a965ada0d64761ed
Marcus Lunden <marcus@thingsquare.com>
2012-10-29 16:50:39 +0100

    more radio defines; needs to be checked

--- platform/launchpad/dev/cc2500-const.h
+++ platform/launchpad/dev/cc2500-const.h
@@ -89,5 +89,80 @@
 #define CC2500_SWORRST  			0x3C
 #define CC2500_SNOP     			0x3D
 
+/* MARCSTATE states */
+// XXX CHECK!
+#if 0
+#define CC2500_STATE_SLEEP             0
+#define CC2500_STATE_IDLE              1
+#define CC2500_STATE_XOFF              2
+#define CC2500_STATE_VCOON_MC          3
+#define CC2500_STATE_REGON_MC          4
+#define CC2500_STATE_MANCAL            5
+#define CC2500_STATE_VCOON             6
+#define CC2500_STATE_REGON             7
+#define CC2500_STATE_STARTCAL          8
+#define CC2500_STATE_BWBOOST           9
+#define CC2500_STATE_FS_LOCK          10
+#define CC2500_STATE_IFADCON          11
+#define CC2500_STATE_ENDCAL           12
+#define CC2500_STATE_RX               13
+#define CC2500_STATE_RX_END           14
+#define CC2500_STATE_RX_RST           15
+#define CC2500_STATE_TXRX_SWITCH      16
+#define CC2500_STATE_RXFIFO_OVERFLOW  17
+#define CC2500_STATE_FSTXON           18
+#define CC2500_STATE_TX               19
+#define CC2500_STATE_TX_END           20
+#define CC2500_STATE_RXTX_SWITCH      21
+#define CC2500_STATE_TXFIFO_UNDERFLOW 22
+#endif
+
+/* State definitions if reading SPI statusbyte; several definitions have the same
+ * value to be compatible with MARCSTATE definition names */
+// XXX CHECK!
+#define CC2500_STATE_RX               1
+#define CC2500_STATE_RX_END           1
+#define CC2500_STATE_TX               2
+#define CC2500_STATE_TX_END           2
+#define CC2500_STATE_FSTXON           3
+#define CC2500_STATE_STARTCAL         4
+#define CC2500_STATE_MANCAL           4
+#define CC2500_STATE_ENDCAL           4
+#define CC2500_STATE_CAL              4
+#define CC2500_STATE_SETTLING         5
+#define CC2500_STATE_RXFIFO_OVERFLOW  6
+#define CC2500_STATE_TXFIFO_UNDERFLOW 7
+#define CC2500_STATUSBYTE_STATUSBITS  0x70
+
+/* Bit fields */
+// XXX CHECK!
+/* invert assert/de-assert */
+#define IOCFG_GPIO_CFG_INVERT             BV(6)
+/* set pin as "Analog transfer" (==pin not used as GPIO) */
+#define IOCFG_GPIO_CFG_ATRAN              BV(7)
+
+/* GPIO functionality */
+/* Assert when THR is reached or EOP; de-assert on empty RxFIFO */
+#define IOCFG_GPIO_CFG_RXFIFO_THR_PKT     1   
+/* assert on SYNC recv/sent, de-assert on EOP */
+#define IOCFG_GPIO_CFG_PKT_SYNC_RXTX      6  
+/* CS valid ? assert : de-assert */
+#define IOCFG_GPIO_CFG_CS_VALID           16  
+/* Carrier Sense ? assert : de-assert */
+#define IOCFG_GPIO_CFG_CS                 17  
+/* assert when in TX, de-assert in Rx/IDLE/settling */ 
+#define IOCFG_GPIO_CFG_RXIDLE_OR_TX       26  
+/* assert if in rx or tx, de-assert if idle/settling (MARC_2PIN_STATUS[0]) */ 
+#define IOCFG_GPIO_CFG_RXTX_OR_IDLE       38
+/* high impedance */
+#define IOCFG_GPIO_CFG_HIGHZ              48  
+
+
+
+
+
+
+
+
 #endif /* __CC2500_H__ */
 

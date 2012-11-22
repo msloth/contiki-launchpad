3ab6459662c2d2969cb972f846ae9b6694b7b911
Marcus Lunden <marcus@thingsquare.com>
2012-11-22 10:26:00 +0100

    removed debugs from rtimer, is now in original Contiki state

--- core/sys/rtimer.c
+++ core/sys/rtimer.c
@@ -52,12 +52,8 @@
 #if DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
-#define PH(x)   P2OUT|=(1<<x)
-#define PL(x)   P2OUT&=~(1<<x)
 #else
 #define PRINTF(...)
-#define PH(x)
-#define PL(x)
 #endif
 
 static struct rtimer *next_rtimer;
@@ -74,7 +70,6 @@ rtimer_set(struct rtimer *rtimer, rtimer_clock_t time,
 	   rtimer_clock_t duration,
 	   rtimer_callback_t func, void *ptr)
 {
-  PH(2);
   int first = 0;
 
   PRINTF("rtimer_set time %d\n", time);
@@ -92,7 +87,6 @@ rtimer_set(struct rtimer *rtimer, rtimer_clock_t time,
   if(first == 1) {
     rtimer_arch_schedule(time);
   }
-  PL(2);
   return RTIMER_OK;
 }
 /*---------------------------------------------------------------------------*/

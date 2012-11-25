a2c2562a54edfd66d0ed1da07715e97bbdd5039c
Marcus Lunden <marcus.lunden@gmail.com>
2012-11-25 01:22:00 +0100

    trivial commit, clean-ups

--- examples/launchpad/button/testbutton.c
+++ examples/launchpad/button/testbutton.c
@@ -9,18 +9,21 @@
 #include <stdio.h>
 #include "contiki.h"
 #include "dev/leds.h"
-#include "button.h"
+#include "dev/button.h"
 
 /*
  * how to read a Contiki program: start by finding AUTOSTART_PROCESSES(...).
  * The first process in that list is started first. Then, if it starts a new
  * process by process_start() execution flow continues in that process until
- * any PROCESS_YIELD_* or PROCESS_WAIT_*.   */
+ * any PROCESS_YIELD_* or PROCESS_WAIT_*.
+ */
 /* -------------------------------------------------------------------------- */
 PROCESS(button_process, "Button process");
 PROCESS(blink_process, "Blink process");
 AUTOSTART_PROCESSES(&blink_process, &button_process);
 /*---------------------------------------------------------------------------*/
+/* This process handles button presses; up until 5 presses it toggles red LED
+after that it kills the blink process if the button is pressed again. */
 static struct etimer etr;
 
 PROCESS_THREAD(button_process, ev, data)
@@ -29,11 +32,11 @@ PROCESS_THREAD(button_process, ev, data)
   PROCESS_EXITHANDLER();
   PROCESS_BEGIN();
   static uint8_t btnpresses = 0;
-  while (1) {
-    PROCESS_WAIT_EVENT_UNTIL(ev == button_event && (*data & SwITCH_2));
+  while(1) {
+    PROCESS_WAIT_EVENT_UNTIL(ev == button_event && (*((uint8_t*)data) & BUTTON_2));
     leds_toggle(LEDS_RED);
     btnpresses++;
-    if (btnpresses == 10) {
+    if(btnpresses == 5) {
       process_exit(&blink_process);
     }
   }
@@ -54,53 +57,3 @@ PROCESS_THREAD(blink_process, ev, data)
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
-#if 0
-
-/*AUTOSTART_PROCESSES(&blink_process, &button_process, &batterycheck_process);*/
-/*PROCESS(batterycheck_process, "Voltage checker");*/
-/* periodically read out supply voltage and print it out on the serial port */
-static struct etimer voltagecheck_timer;
-static uint16_t battval;
-
-PROCESS_THREAD(batterycheck_process, ev, data) {
-  PROCESS_POLLHANDLER();
-  PROCESS_EXITHANDLER();
-  PROCESS_BEGIN();
-
-  SENSORS_ACTIVATE(battery_sensor);
-
-  while (1) {
-    etimer_set(&voltagecheck_timer, CLOCK_SECOND);
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&voltagecheck_timer));
-    /* read voltage (10bit = 0..1024), convert to mV */
-    battval = (uint16_t)((battery_sensor.value(0) * 1000 * 5) / (1024*3));
-    printf("[%u] %u mV\n", (uint16_t)clock_seconds(), battval);
-  }
-  
-  PROCESS_END();
-}
-#endif
-
-
-
-#if 0
-/*---------------------------------------------------------------------------*/
-static struct etimer etr;
-static uint8_t btnpresses = 0;
-PROCESS_THREAD(button_process, ev, data)
-{
-  PROCESS_POLLHANDLER();
-  PROCESS_BEGIN();
-  SENSORS_ACTIVATE(button_sensor);
-  while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
-    leds_toggle(LEDS_GREEN);
-    btnpresses++;
-    if (btnpresses == 10) {
-      process_exit(&blink_process);
-    }
-  }
-  PROCESS_END();
-}
-
-#endif

ac70c8be2083b465f6d8927f111d0f7a9e451171
Marcus Lunden <marcus.lunden@gmail.com>
2012-11-25 01:18:33 +0100

    fixed pwm finally

--- platform/launchpad/contiki-launchpad-main.c
+++ platform/launchpad/contiki-launchpad-main.c
@@ -1,32 +1,3 @@
-/*
- * Copyright (c) 2006, Swedish Institute of Computer Science
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- */
 
 #include <stdio.h>
 #include <string.h>
@@ -39,11 +10,12 @@
 #include "dev/uart0.h"
 #include "dev/watchdog.h"
 #include "sys/autostart.h"
-#include "dev/button.h"
-#include "dev/adc.h"
 #include "lib/random.h"
 #include "net/rime.h"
 #include "netstack.h"
+#include "dev/button.h"
+#include "dev/adc.h"
+#include "dev/pwm.h"
 
 /*---------------------------------------------------------------------------*/
 /*  P1SEL &=~ (LEDS_RED | LEDS_GREEN);*/
@@ -64,11 +36,18 @@ main(void)
 
   #if USE_SERIAL
   /* 
-   * The Launchpad is limited to 9600 by the msp430-usb bridge; higher speeds can
+   * The Launchpad is limited to 9600 by the msp430--usb bridge; higher speeds can
    * be used with a separate uartserial->usb cable connected to the rxtx pins on
-   * the header, but this is kept at a maximum simplicity now, hence 9600.
+   * the header, but this Contiki port is kept at a maximum simplicity now, hence
+   * 9600. If you do get one such cable, you can increase this to eg 115200.
    */
   uart0_init(BAUD2UBR(9600));
+  #else
+  /*
+   * any printf's makes compiler complain of unresolved references to putchar;
+   * this solves that. Must come before first printf.
+   */
+  #define printf(...)
   #endif  /* USE_SERIAL */
 
   rtimer_init();
@@ -78,6 +57,13 @@ main(void)
 
   button_init();
   adc_init();
+
+  #if _MCU_ == 2553
+  #if HAS_EXT_OSC
+  /* pwm only available on 2553 with external crystal as it has two hw timers */
+  pwm_init();
+  #endif    /* HAS_EXT_OSC */
+  #endif    /* _MCU_ == 2553 */
 
   #if USE_SERIAL
   uart0_set_input(serial_line_input_byte);
@@ -107,17 +93,20 @@ main(void)
   }
 
   netstack_init();
+
+  //XXX sth in this clause (radio driver?) messes with LEDs; find and fix
+  leds_init();  // XXX remove when fixed.
   #endif  /* USE_RADIO */
 
-  printf(CONTIKI_VERSION_STRING " started. ");
   watchdog_start();
   autostart_start(autostart_processes);
-
   leds_off(LEDS_ALL);
+  printf(CONTIKI_VERSION_STRING " started. ");
 
   while(1) {
-    /* The Contiki main loop, greatly simplified and shortened compared with eg
-     * msp430f1611 due to severe memory constraints (mainly RAM).
+    /*
+     * The Contiki main loop, greatly simplified and shortened compared with eg
+     * msp430f1611 due to severe space constraints (mainly RAM).
      * As soon as we are not doing anything, we spend the time in LPM3.
      */
     int r;
@@ -127,6 +116,7 @@ main(void)
       r = process_run();
     } while(r > 0);
 
+    /* if not printing or pending events, sleep. */
     #if USE_SERIAL
       if(process_nevents() == 0 && !uart0_active()) {
     #else
--- platform/launchpad/dev/pwm.c
+++ platform/launchpad/dev/pwm.c
@@ -1,142 +1,247 @@
-
-#include <stdio.h>
 #include "contiki.h"
 #include "isr_compat.h"
 #include "pwm.h"
+#include "dev/leds.h"
+
 
 /*
+ * The PWM module; starts and handles PWM on pins. 
+ * 
+ * Assumes PWM freq of no slower than 128 Hz (7.8 ms period due to otherwise
+ * variables have to be 16-bit so we save some RAM this way.
+ * 
+ * There are checks for MCU being an MSP430G2553 as the '2452 does not have two
+ * hardware timers, which this module needs.
+ */
 
+/*---------------------------------------------------------------------------*/
+/*
   XXX todo/suggestions
   * change clock and rtimer to TimerA1? This as TimerA0 has output (PWM) pins on port 1 and TimerA1 on port 2.
-  * invert pin mode/dc=255-dc
   * smoothly change PWM over time, PI-controller
+      --ie set eg rise time and 'should'-value
       --build separate module for this
   * servo-motor module
       --build separate module for this
-      --cannot co-exist with PWM as not enough hardware timers (messy otherwise)
-        and 20 ms period time is 50Hz/655.26 ticks @ 32768.
-        One tick is 0.0305 ms, too slow for servo arm position fine resolution?
-        50Hz is ok for LED, is it good enough for servos?
- */
+      --One tick is 0.0305 ms, too low res for servo arm position fine resolution?
+        ->ca 5.5 deg resolution
+      --no, this can be used for that if freq set to 50..100 Hz and on_time set
+        to ca 25..65 which gives an angular resolution of ca 5.5 degrees.
+        For finer control, the timer must be sourced from DCO which means that
+        the microcontroller can't go to LPM3.
+*/
+/*---------------------------------------------------------------------------*/
+
+/* PWM freq; can be higher and lower; setting to lower will increase accuracy of
+  the duty cycle (rounding errors) calculated in pwm_on(). */
+#define PWM_FREQ          128
+/* PWM module is sourced from external crystal osc @ 32.768 kHz, hence do not change this */
+#define PWM_CLOCK_SPEED   32768
 
 /*
- * The PWM module; starts and handles PWM on pins. The duty cycle can be chosen 
- * from [1..255] or off.
- * 
- * This is kept as simple as possible to keep RAM usage down. 
- * 
+ * a PWM-device is here a combination of a pin and an time for which to be in 
+ * 'on'-state. If time is zero, then the device is off. All PWM-devices has a
+ * common period. Each PWM-device correspond to a CCR-register, for which the 
+ * 2553 has three, but one is for the common period. 
  */
+#define PWMDEVS   2     /* number of PWM devices; must not be changed */
+struct pwm_s {
+  /* pin is pin on port 1 */
+  uint8_t pin;
+  /* on_time is time in on-state; duty cycle == on_time/period */
+  uint8_t on_time;
 
+  /* possible enhancement 1: store port as well to enable PWM on several ports */
+/*  char *port;*/
+  
+  /* possible enhancement 2: allows for more a more generic approach with more/less
+  PWM-devices, but takes more RAM: store pointers to registers */
+/*  char *reg_ctl;*/
+/*  char *reg_ccr;*/
 
+} pwms[PWMDEVS];
+
+const uint16_t period = (PWM_CLOCK_SPEED / PWM_FREQ) - 1;    /* don't change */
 /*---------------------------------------------------------------------------*/
-/* keep settings for a maximum of 8 pins = one port. Seems like the most space
-efficient way of doing it. */
-#define MAX_PWM_PINS  8
-static uint8_t pwmsettings[MAX_PWM_PINS];
-/*---------------------------------------------------------------------------*/
-/* Init the PWM */
+/* Init the PWM; up mode to CCR0, PWM periods CCR1, CCR2, clock source XT1 @ 32.768 */
 void
 pwm_init(void)
 {
-  uint8_t i;
-  for (i = 0; i < MAX_PWM_PINS; i += 1) {
-    pwmsettings[i] = 0;
-  }
-
+#if _MCU_ == 2553
   dint();
-  /* Timer is in cont up mode, irq when counter TAR == CCR0 */
-  /* using ACLK, which is 32768 ext osc; div 1 --> 32.768kHz; with a 256 period this is 128 Hz,
-  enough for LEDs but not for servo motors. */
-  TA1CTL = TASSEL_1 | ID_0;
-  TA1CCR0 = ;
+
+  /* null out both PWM-devices */
+  pwms[0].pin = 0;
+  pwms[0].on_time = 0;
+  pwms[1].pin = 0;
+  pwms[1].on_time = 0;
+
+  /* count up to CCR0 == period, clock source external crystal @ 32.768 kHz */
   TA1CCTL0 = CCIE;
-  TA1CTL |= MC_2;
-  count = 0;
+  TA1CCR0 = period;
+  TA1CTL = TASSEL_1 | ID_0;
+  TA1CTL |= MC_1;
+
   eint();
+#endif    /* _MCU_ == 2553 */
 }
 /*--------------------------------------------------------------------------*/
-/* turn PWM on on a pin; dc is duty cycle expressed in [1..255] where 255 is max
-and pin is pin [0..7] on port 1 to run PWM on. */
+/* Turn on PWM on a PWM-device, for a certain pin on port 1, at a duty cycle dc;
+note, this makes an calculation with a division so it might be slow. Use 
+pwm_on_fine if possible/necessary/fun. */
 void
-pwm_on(uint8_t dc, uint8_t pin)
+pwm_on(uint8_t pwmdevice, uint8_t pin, uint8_t dc)
 {
-  if(pin > 7 || dc == 0) {
+#if _MCU_ == 2553
+  if(pin > 7 || dc == 0 || dc > 100 || pwmdevice >= PWMDEVS) {
+    return;
+
+  } else {
+    /* convert duty cycle to PWM clock ticks */
+    uint8_t finetime = (dc * period) / 100;
+
+    /* set PWM accordingly */
+    if(pwmdevice == 0) {
+      TA1CCR1 = finetime;
+      TA1CCTL1 = CCIE;
+    } else {
+      TA1CCR2 = finetime;
+      TA1CCTL2 = CCIE;
+    }
+
+    /* set up a compare match for the next PWM period */
+    pwms[pwmdevice].pin = pin;
+    pwms[pwmdevice].on_time = finetime;
+  }
+#endif    /* _MCU_ == 2553 */
+}
+/*--------------------------------------------------------------------------*/
+/* For finer control, this function can be used to set duty time in ticks instead
+of a duty cycle in percent, eg for servo motor control. */
+void
+pwm_on_fine(uint8_t pwmdevice, uint8_t pin, uint8_t finetime)
+{
+#if _MCU_ == 2553
+  /* sanity checks */
+  if(pin > 7 || finetime == 0 || finetime > period || pwmdevice >= PWMDEVS) {
     return;
   }
-  pwmsettings[pin] = dc;
-  TA1CCR1 = dc;
+
+  /* set up a compare match for the next PWM period */
+  if(pwmdevice == 0) {
+    TA1CCR1 = finetime;
+    TA1CCTL1 = CCIE;
+  } else {
+    TA1CCR2 = finetime;
+    TA1CCTL2 = CCIE;
+  }
+  pwms[pwmdevice].pin = pin;
+  pwms[pwmdevice].on_time = finetime;
+#endif    /* _MCU_ == 2553 */
 }
 /*--------------------------------------------------------------------------*/
-/* returns the current duty cycle on the pin [0..7] on port1 */
-uint8_t
-pwm_get(uint8_t pin)
+/* get the period in ticks sourced at 32768 Hz. */
+uint16_t
+pwm_period(void)
 {
-  if(pin > 7) {
-    /* illegal pin, OOB */
+#if _MCU_ == 2553
+  return period;
+#else    /* _MCU_ == 2553 */
+  return 0;
+#endif    /* _MCU_ == 2553 */
+}
+/*--------------------------------------------------------------------------*/
+void
+pwm_off(uint8_t pwmdevice)
+{
+#if _MCU_ == 2553
+  if(pwmdevice >= PWMDEVS) {
+    return;
+  }
+
+  if(pwmdevice == 0) {
+    TA1CCTL1 &= ~(CCIE);
+  } else {
+    TA1CCTL2 &= ~(CCIE);
+  }
+  P1OUT &= ~(1 << pwms[pwmdevice].pin);
+  pwms[pwmdevice].on_time = 0;
+#endif    /* _MCU_ == 2553 */
+}
+/*--------------------------------------------------------------------------*/
+/* returns the current duty cycle of a PWM device */
+// XXX incorrect, currently returns the on-time, not dc.
+uint8_t
+pwm_dc(uint8_t pwmdevice)
+{
+#if _MCU_ == 2553
+  if(pwmdevice >= PWMDEVS) {
     return 0;
   }
-  return pwmsettings[pin];
+  return pwms[pwmdevice].on_time;
+#else    /* _MCU_ == 2553 */
+  return 0;
+#endif    /* _MCU_ == 2553 */
 }
 /*--------------------------------------------------------------------------*/
+/* Switch off all PWM-devices */
 void
 pwm_all_off(void)
 {
-  for (i = 0; i < MAX_PWM_PINS; i += 1) {
-    pwmsettings[i] = 0;
+#if _MCU_ == 2553
+  if(pwms[0].on_time != 0) {
+    P1OUT &= ~(1 << pwms[0].pin);
+    pwms[0].on_time = 0;
   }
+  TA1CCTL1 &= ~(CCIE);
+
+  if(pwms[1].on_time != 0) {
+    P1OUT &= ~(1 << pwms[1].pin);
+    pwms[1].on_time = 0;
+  }
+  TA1CCTL2 &= ~(CCIE);
+#endif    /* _MCU_ == 2553 */
 }
 /*--------------------------------------------------------------------------*/
-/* ISR for CCR0 compare match */
+/* ISR for CCR0 compare match ie end of a period */
+#if _MCU_ == 2553
 ISR(TIMER1_A0, pwm_periodstart_ta1ccr0_isr)
 {
-  ;
-}
-/*---------------------------------------------------------------------------*/
-/* ISR for CCR1..2 compare match */
-ISR(TIMER1_A1, pwm_ccrmatch_ta1ccrX_isr)
-{
-  ;
-}
-/*---------------------------------------------------------------------------*/
-#if 0
-  #define TIMER0_A1_VECTOR    (0x0010)  /* 0xFFF0 Timer0_A CC1, TA0 */
-  #define TIMER0_A0_VECTOR    (0x0012)  /* 0xFFF2 Timer0_A CC0 */
-  #define TIMER1_A1_VECTOR    (0x0018) /* 0xFFF8 Timer1_A CC1-4, TA1 */
-  #define TIMER1_A0_VECTOR    (0x001A) /* 0xFFFA Timer1_A CC0 */
+  /* ensure next period interrupt happens at correct time */
+  TA1CCTL0 &= ~CCIFG;
 
-  #define TACCTL0             TA0CCTL0  /* Timer A Capture/Compare Control 0 */
-  #define TACCTL1             TA0CCTL1  /* Timer A Capture/Compare Control 1 */
-  #define TACCTL2             TA0CCTL2  /* Timer A Capture/Compare Control 2 */
-#endif
-/*--------------------------------------------------------------------------*/
-#if NOTINUSE
-#define MCU_CLOCK                       1000000
-#define PWM_FREQUENCY                   46              // In Hertz, ideally 50Hz.
-
-unsigned int PWM_Period         = (MCU_CLOCK / PWM_FREQUENCY);  // PWM Period
-unsigned int PWM_Duty           = 0;                                                    // %
-
-void main (void){
-  TACCTL1 = OUTMOD_7;            // TACCR1 reset/set
-  TACTL   = TASSEL_2 + MC_1;     // SMCLK, upmode
-  TACCR0  = PWM_Period-1;        // PWM Period
-  TACCR1  = PWM_Duty;            // TACCR1 PWM Duty Cycle
-
-  P1DIR   |= BIT2;               // P1.2 = output
-  P1SEL   |= BIT2;               // P1.2 = TA1 output
-
-  while (1){
-    PROCESS_WAIT_EVENT_UNTIL(ev == servo_change_event);
-    pwm_set(SERVO_PWM_PIN, servo_lut[45]);
+  /* clear PWM-devices output pins */
+  if(pwms[0].on_time != 0) {
+    P1OUT &= ~(1 << pwms[0].pin);
+  }
+  if(pwms[1].on_time != 0) {
+    P1OUT &= ~(1 << pwms[1].pin);
   }
 }
-#endif
+#endif    /* _MCU_ == 2553 */
+/*---------------------------------------------------------------------------*/
+/* ISR for CCR1, CCR2 compare match ie end of a PWM on period */
+#if _MCU_ == 2553
+ISR(TIMER1_A1, pwm_ccrmatch_ta1ccrX_isr)
+{
+  /* store the IV register as any read/write resets interrupt flags */
+  uint16_t ivreg = TA1IV;
+  if(ivreg & TA1IV_TACCR1) {
+    /* ensure next PWM interrupt happens at correct time */
+    TA1CCTL1 &= ~CCIFG;
+    /* set output */
+    if(pwms[0].on_time != 0) {
+      P1OUT |= (1 << pwms[0].pin);
+    }
 
-
-
-
-
-
-
-
-
+  } else if(ivreg & TA1IV_TACCR2) {
+    /* ensure next PWM interrupt happens at correct time */
+    TA1CCTL2 &= ~CCIFG;
+    /* set output */
+    if(pwms[1].on_time != 0) {
+      P1OUT |= (1 << pwms[1].pin);
+    }
+  }
+}
+#endif    /* _MCU_ == 2553 */
+/*---------------------------------------------------------------------------*/

49429dcbcdfddd0165aec90492a0c5c9155eb910
Marcus Lunden <marcus.lunden@gmail.com>
2012-12-12 22:28:34 +0100

    added test example of alphanumeric displays

---
+++ examples/launchpad/alphanum-test/alphanumtest.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2012
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/**
+ * \file
+ *         Test Sparkfun alphanumeric display
+ * \author
+ *         Marcus Lunden <marcus.lunden@gmail.com>
+ */
+
+#include <stdio.h>
+#include "contiki.h"
+#include "dev/leds.h"
+#include "dev/alphanumeric.h"
+/* -------------------------------------------------------------------------- */
+PROCESS(red_process, "LED+alphanum process");
+AUTOSTART_PROCESSES(&red_process);
+/*---------------------------------------------------------------------------*/
+static uint8_t cnt = 0;
+static struct etimer etr;
+PROCESS_THREAD(red_process, ev, data)
+{
+  PROCESS_POLLHANDLER();
+  PROCESS_EXITHANDLER();
+  PROCESS_BEGIN();
+  alphanum_init();
+/*  alphanum_print_char('i');*/
+/*  alphanum_print_char('H');*/
+  alphanum_print_string("Hi");
+  while(1) {
+    leds_toggle(LEDS_RED);
+/*    alphanum_print_char(cnt);*/
+/*    alphanum_shift(0);*/
+/*    alphanum_shift(1<<cnt);*/
+/*    alphanum_shift(1<<cnt);*/
+    cnt++;
+    if(cnt >= 15) {
+      cnt = 0;
+    }
+    etimer_set(&etr, CLOCK_SECOND/2);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&etr));
+  }
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+

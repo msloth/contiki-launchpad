a7f887caafe49263a2e72fff3bedd5006a046a18
Marcus Lunden <marcus@thingsquare.com>
2012-12-13 17:56:41 +0100

    added a sinus(x) lookup-table that will be used later

--- examples/launchpad/pwm-led/pwm-led.c
+++ examples/launchpad/pwm-led/pwm-led.c
@@ -55,21 +55,33 @@
 
 /* wait this long between setting a new PWM setting */
 #define INTERVAL          CLOCK_SECOND/64
+
+/*
+ * lookup-table of sin(x) where x == [0 .. 1.5 .. 90], 63 elements but I added  
+ * one extra 255 to get 64 elements.
+ */
+const uint8_t sin_lut[] = {0, 2, 4, 6, 13, 20, 26, 33, 40, 46, 53, 59, 66, 72, 79, 85, 91, 97, 
+    104, 110, 116, 122, 127, 133, 139, 144, 150, 155, 161, 166, 171, 176, 181, 
+    185, 190, 194, 198, 203, 207, 210, 214, 218, 221, 224, 228, 231, 233, 236,
+    238, 241, 243, 245, 247, 248, 250, 251, 252, 253, 254, 255, 255, 255, 255,
+    255};
+
+#define SIN_MAXELEMENTS    45
 /*---------------------------------------------------------------------------*/
 PROCESS(button_process, "Button reader");
 PROCESS(pwmled_process, "PWM LED process");
 AUTOSTART_PROCESSES(&button_process, &pwmled_process);
 /*---------------------------------------------------------------------------*/ 
 /* set the PWM duty cycle on the pins (ie the LEDs) out of phase from eachother */
-static struct etimer etr;
-static uint8_t i = 1;     /* counter */
-static uint8_t up = 1;    /* counting up or down? */
 
 PROCESS_THREAD(pwmled_process, ev, data)
 {
   PROCESS_POLLHANDLER();
   PROCESS_EXITHANDLER(pwm_all_off(); etimer_stop(&etr););
   PROCESS_BEGIN();
+  static struct etimer etr;
+  static uint8_t i = 1;     /* counter */
+  static uint8_t up = 1;    /* counting up or down? */
 
   pwm_confpin(LEDGRN_PIN);
   pwm_confpin(LEDRED_PIN);

0ee50e172d79ed9eb2854f53caf445c826f66888
Marcus Lunden <marcus.lunden@gmail.com>
2013-04-19 09:54:24 +0200

    added script for generation of sinus LUT

---
+++ examples/launchpad/clock-hpdl1414/sin-lut-generator.py
@@ -0,0 +1,15 @@
+import math
+
+min = 0
+max = 90
+steps = 45
+
+sinlut = "const uint8_t sin_lut[] = {"
+for x in xrange(min, max, max/steps):
+	val = 100 * math.sin(math.pi*x/180)
+	sinlut += str(int(val)) + ", "
+	#print(str(int(100 * math.sin(math.pi*x/180)))),
+
+sinlut += '}'
+
+print(sinlut)

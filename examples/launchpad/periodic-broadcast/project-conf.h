b7106535088ca50ce16b4c61a0607d91228ea582
Marcus Linderoth <marcus@thingsquare.com>
2016-06-10 15:36:11 +0200

    use project-conf instead of platform conf copy

---
+++ examples/launchpad/periodic-broadcast/project-conf.h
@@ -0,0 +1,11 @@
+#ifndef __PLATFORM_CONF_H__
+#define __PLATFORM_CONF_H__
+/*---------------------------------------------------------------------------*/
+/* see platform-conf.h for more details */
+#define F_CPU                   8000000uL
+#define _MCU_                   2553
+#define USE_SERIAL              0
+#define USE_RADIO               1
+#define BOARD_OLD_REVISION      0
+/*---------------------------------------------------------------------------*/
+#endif /* __PLATFORM_CONF_H__ */
